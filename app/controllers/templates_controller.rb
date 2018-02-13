a552190a0f9e08c628d50f4f0dffa804a5aaa8ba
yui <b1cuyr@gmail.com>
2018-02-13 14:03:14 +0000

    cocoon実装

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,18 +1,19 @@
 class TemplatesController < ApplicationController
     
     def new
-        @template = current_user.templates.build
+        @template = current_user.templates.new
         @template.template_items.build
     end
     
     
     def create
+        binding.pry
         Template.create(template_params)
     end
     
     private 
     def template_params
-        params.require(:template).permit(:name, :color, :user_id, template_items_attributes: [:itemname])
+        params.require(:template).permit(:name, :color, :user_id, template_items_attributes: [:itemname, :_destroy])
     end
     
 end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,5 +1,5 @@
 class Template < ActiveRecord::Base
     belongs_to :user
-    has_many :template_items #throughでitemoption定義？
-    accepts_nested_attributes_for :template_items   #親子関係
+    has_many :template_items #throughでoption定義?
+    accepts_nested_attributes_for :template_items, allow_destroy: true   #親子関係
 end
