361d6b3e5cec6cd0cf668cd036be1c9fb25372a2
yui <b1cuyr@gmail.com>
2018-02-19 15:04:55 +0000

    0219

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,7 @@ class ReviewsController < ApplicationController
         @templates = Template.where(current_user.id).order('created_at asc')
         if params[:template_id].present?
             @reviews = Review.where(template_id: params[:template_id]).order('created_at desc')
+            @template_id = params[:template_id]
         end
     end
     
